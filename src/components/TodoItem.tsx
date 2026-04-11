23ab0bd038c1c2ebc1b2e2dcc7ee83867a50b76c
leonardoSzarblewski <leonardo27.laurindo@gmail.com>
2026-04-11 18:24:49 -0300

    refactor: clean up TodoItem component formatting and add alert in button complete and remove

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,36 +1,41 @@
 import { Link } from "react-router";
 
-import  TodoItemStyles  from "./TodoItem.module.css"
+import TodoItemStyles from "./TodoItem.module.css"
 
 type todoItemProps = {
-    id: string,
-    label: string,
-    complete: boolean,
-    onComplete(): void
-    onRemove(): void
+  id: string,
+  label: string,
+  complete: boolean,
+  onComplete(): void
+  onRemove(): void
 }
 
 export function TodoItem({ id, label, complete, onComplete, onRemove }: todoItemProps) {
-    
 
- return(
-       
+
+  return (
+
     <li key={id} className={TodoItemStyles.Item} data-complete={complete}>
-      <Link to={ `/detalhe/${id}` } className={TodoItemStyles.Text}>
+      <Link
+        to={`/detalhe/${id}`}
+        className={TodoItemStyles.Text}
+        style={{ textDecoration: complete ? "line-through" : "none", color: "Black" }}>
+
         {label}
+
       </Link>
 
       <div className={TodoItemStyles.ButtonsGroup}>
         {!complete && (
-          <button onClick={onComplete} className={TodoItemStyles.ButtonComplete}>
+          <button onClick={() => { onComplete(); alert("Tarefa concluída!"); }} className={TodoItemStyles.ButtonComplete}>
             Concluir
           </button>
         )}
-        <button onClick={onRemove} className={TodoItemStyles.ButtonRemove}>
+        <button onClick={() => { onRemove(); alert("Tarefa removida!"); }} className={TodoItemStyles.ButtonRemove}>
           Remover
         </button>
       </div>
     </li>
-       
-) 
+
+  )
 }
